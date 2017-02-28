3e4c6b9e976a0fd00178b95d025a4f9d6aaa9138
Anton Virtanen <avirtanen@hotmail.fr>
2017-02-28 15:52:37 +0100

    done excpet styling

--- starter-code/contact-app/src/app/contact-list/contact-list.component.ts
+++ starter-code/contact-app/src/app/contact-list/contact-list.component.ts
@@ -4,11 +4,14 @@ import contactList from '../contacts';
 @Component({
   selector: 'app-contact-list',
   templateUrl: './contact-list.component.html',
-  styleUrls: ['./contact-list.component.css']
+  styleUrls: ['./contact-list.component.css'],
+
 })
+
 export class ContactListComponent implements OnInit {
   contacts: Object[];
   newContact: Object = {};
+  pattern: string;
 
   constructor() { }
 
@@ -19,6 +22,8 @@ export class ContactListComponent implements OnInit {
   addContact(){
     console.log("Add contact has been called");
     // add contact to contacts list
+     this.contacts.push(this.newContact);
     // clear inputs
+    this.newContact = {};
   }
 }
