1b548334893fad30fd4b8596aea51a018b1376dd
Fran Bosquet <frbosquet@gmail.com>
2017-07-25 12:25:57 +0200

    Done

--- starter-code/contact-app/src/app/contact-list/contact-list.component.ts
+++ starter-code/contact-app/src/app/contact-list/contact-list.component.ts
@@ -20,6 +20,14 @@ export class ContactListComponent implements OnInit {
   addContact(){
     console.log("Add contact has been called right now");
     // add contact to contacts list
+    let newContact = {
+      name : this.newContact['name'],
+      email : this.newContact['email'],
+      phoneNumber : this.newContact['phoneNumber'],
+      image : this.newContact['image'] || "http://lorempixel.com/640/480/cats"
+    }
+    contacts.push(newContact);
     // clear inputs
+    this.newContact = {};
   }
 }
