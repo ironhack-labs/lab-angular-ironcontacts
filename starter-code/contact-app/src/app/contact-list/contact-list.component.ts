94ac24e17d4741b813bf92a688b99656b51509ed
Fran Bosquet <frbosquet@gmail.com>
2017-07-25 11:48:55 +0200

    Iteration 1

--- starter-code/contact-app/src/app/contact-list/contact-list.component.ts
+++ starter-code/contact-app/src/app/contact-list/contact-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import contactList from '../contacts';
+import contacts from '../contacts';
 
 @Component({
   selector: 'app-contact-list',
@@ -7,7 +8,7 @@ import contactList from '../contacts';
   styleUrls: ['./contact-list.component.css']
 })
 export class ContactListComponent implements OnInit {
-  contacts: Object[];
+  contacts: Object[] = contacts;
   newContact: Object = {};
 
   constructor() { }
@@ -17,7 +18,7 @@ export class ContactListComponent implements OnInit {
   }
 
   addContact(){
-    console.log("Add contact has been called");
+    console.log("Add contact has been called right now");
     // add contact to contacts list
     // clear inputs
   }
