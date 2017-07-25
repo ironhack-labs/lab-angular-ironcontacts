0d05efd8f461f18a528b69d515cedc2154f8126e
Monica Calderaro <monicacalderaro@Monicas-MacBook-Air.local>
2017-07-25 12:11:31 +0200

    iteration 2 done

--- starter-code/contact-app/src/app/contact-list/contact-list.component.ts
+++ starter-code/contact-app/src/app/contact-list/contact-list.component.ts
@@ -10,15 +10,17 @@ export class ContactListComponent implements OnInit {
   contacts: Object[];
   newContact: Object = {};
 
+
   constructor() { }
 
   ngOnInit() {
     this.contacts = contactList;
   }
 
-  addContact(){
-    console.log("Add contact has been called");
+  addContact(clear){
     // add contact to contacts list
+    this.contacts.push(this.newContact);
     // clear inputs
+    this.newContact = {};
   }
 }
