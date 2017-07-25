cd9b76608a0b32a327e72ac42624c4506453113d
Emilie G <adamanska@gmail.com>
2017-07-25 15:37:09 +0200

    ite 2 in process

--- starter-code/contact-app/src/app/contact-list/contact-list.component.ts
+++ starter-code/contact-app/src/app/contact-list/contact-list.component.ts
@@ -17,9 +17,9 @@ export class ContactListComponent implements OnInit {
   }
 
   addContact(){
-    console.log("Add contact has been called");
+    // console.log("Add contact has been called");
     // add contact to contacts list
-    
+    this.contacts.push(this.emiContact);
     // clear inputs
   }
 }
