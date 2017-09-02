eb0b1f94b557426e64a4e044af3870c9c3019a40
zocha_sz <zofia.szczudlik@gmail.com>
2017-09-02 16:49:08 +0200

    Done

--- starter-code/contact-app/src/app/contact-list/contact-list.component.ts
+++ starter-code/contact-app/src/app/contact-list/contact-list.component.ts
@@ -16,9 +16,10 @@ export class ContactListComponent implements OnInit {
     this.contacts = contactList;
   }
 
-  addContact(){
+  addContact(contact: Object): void{
     console.log("Add contact has been called");
-    // add contact to contacts list
-    // clear inputs
+
+    this.contacts.push(this.newContact);
+    this.newContact = {};
   }
 }
