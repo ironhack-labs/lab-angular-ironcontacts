9bc306153f86e7b1756e52b26186065f34efd6f2
Jordi Piella <jordipiella@gmail.com>
2017-09-16 13:10:09 +0200

    Done

--- starter-code/contact-app/src/app/contact-list/contact-list.component.ts
+++ starter-code/contact-app/src/app/contact-list/contact-list.component.ts
@@ -7,18 +7,47 @@ import contactList from '../contacts';
   styleUrls: ['./contact-list.component.css']
 })
 export class ContactListComponent implements OnInit {
-  contacts: Object[];
-  newContact: Object = {};
+  contacts: any[];
+  newContact: any = {};
 
   constructor() { }
 
   ngOnInit() {
     this.contacts = contactList;
   }
-
+  
   addContact(){
     console.log("Add contact has been called");
     // add contact to contacts list
+    this.contacts.push(this.newContact);
     // clear inputs
+    console.log(this.contacts);
+    this.newContact = {};
+    
+  }
+  
+}
+
+
+/*
+let x = 0;
+let y = 1;
+let z = 2;
+class object {
+  constructor(a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
   }
 }
+
+const what = new object(x, y, z)
+class object {
+  costructor(a, b, c) {
+    this.a = b;
+    this.b = c;
+    this.c = a;
+  }
+}
+console.log(what.a)
+*/
