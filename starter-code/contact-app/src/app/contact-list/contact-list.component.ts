5bffda57418fecd5179af1ff1cb5ffcc0de3054b
Marc Nicolau <info@marcnicolau.net>
2017-09-16 14:52:42 +0200

    done

--- starter-code/contact-app/src/app/contact-list/contact-list.component.ts
+++ starter-code/contact-app/src/app/contact-list/contact-list.component.ts
@@ -9,7 +9,14 @@ import contactList from '../contacts';
 export class ContactListComponent implements OnInit {
   contacts: Object[];
   newContact: Object = {};
-
+  nameValidation: String = "";
+  messageName: String = "";
+  emailValidation: String = "";
+  messageEmail: String = "";
+  phoneValidation: String = "";
+  messagePhone: String = "";
+  urlValidation: String = "";
+  messageUrl: String = "";
   constructor() { }
 
   ngOnInit() {
@@ -18,7 +25,48 @@ export class ContactListComponent implements OnInit {
 
   addContact(){
     console.log("Add contact has been called");
-    // add contact to contacts list
-    // clear inputs
+    this.contacts.push(this.newContact);
+    this.newContact = {};
+  }
+
+  validateName(event) {
+    let rgExp = new RegExp("^[a-zA-Z\s]*$");
+    if(rgExp.test(event.target.value)) {
+      this.messageName = "Text OK"
+      this.nameValidation = "text-success";
+    } else {
+      this.messageName = "Text Error"
+      this.nameValidation = "text-danger";
+    }
+  }
+  validateEmail(event) {
+    let rgExp = new RegExp('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$');
+    if(rgExp.test(event.target.value)) {
+      this.emailValidation = "text-success";
+      this.messageEmail = "Email OK"
+    } else {
+      this.emailValidation = "text-danger";
+      this.messageEmail = "Email Error"
+    }
+  }
+  validatePhone(event) {
+    let rgExp = new RegExp('^[0-9\s]*$');
+    if(rgExp.test(event.target.value)) {
+      this.phoneValidation = "text-success";
+      this.messagePhone = "Phone OK"
+    } else {
+      this.phoneValidation = "text-danger";
+      this.messagePhone = "Phone Error"
+    }
+  }
+  validateURL(event) {
+    let rgExp = new RegExp('(http|ftp|https)://[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:/~+#-]*[\w@?^=%&amp;/~+#-])?');
+    if(rgExp.test(event.target.value)) {
+      this.urlValidation = "text-success";
+      this.messageUrl = "URL OK"
+    } else {
+      this.urlValidation = "text-danger";
+      this.messageUrl = "URL Error"
+    }
   }
 }
