2318fbb5164c3042a6d3e174e8d0b5e1060c505a
Joao Campos <j.campos4g@gmail.com>
2017-09-26 12:44:44 -0400

    best styling ever

--- starter-code/contact-app/src/app/app.component.ts
+++ starter-code/contact-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { contacts} from './contacts';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  fakeContacts: Object[] = contacts;
+
+  title = 'Fake Contacts Jurassic Park';
 }
