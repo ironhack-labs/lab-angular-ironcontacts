3992354c73a5c37ccbfbb75595951c7ce631a11b
Eloi Gil <eloigil6@gmail.com>
2017-11-28 14:50:59 +0100

    BCN - Juan & Eloi

--- starter-code/contact-app/src/app/contact-list/contact-list.component.ts
+++ starter-code/contact-app/src/app/contact-list/contact-list.component.ts
@@ -8,7 +8,12 @@ import contactList from '../contacts';
 })
 export class ContactListComponent implements OnInit {
   contacts: Object[];
-  newContact: Object = {};
+  newContact: Object = {
+    name: null,
+    email: null,
+    phoneNumber: null,
+    image: null
+  };
 
   constructor() { }
 
@@ -16,9 +21,11 @@ export class ContactListComponent implements OnInit {
     this.contacts = contactList;
   }
 
-  addContact(){
+  addContact() {
     console.log("Add contact has been called");
     // add contact to contacts list
     // clear inputs
+
+    this.contacts.push(this.newContact);
   }
 }
