0a4a10f2f3de6aff9440d38c32f573f37bf309b0
Pablo Gutierrez Barcelo <pablo@gutierrezbarcelo.com>
2017-11-28 12:01:57 +0100

    Iteration 2 finished

--- starter-code/contact-app/src/app/contact-list/contact-list.component.ts
+++ starter-code/contact-app/src/app/contact-list/contact-list.component.ts
@@ -17,7 +17,7 @@ export class ContactListComponent implements OnInit {
   }
 
   addContact(){
-    console.log("Add contact has been called");
+    this.contacts.push(this.newContact);
     // add contact to contacts list
     // clear inputs
   }
