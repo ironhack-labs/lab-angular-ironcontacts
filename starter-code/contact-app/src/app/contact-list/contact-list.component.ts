70aa74d6933dae31de665a87a9df6ecd791b04aa
Beltran Rengifo <merloc@gmail.com>
2018-02-20 11:46:38 +0100

    done

--- starter-code/contact-app/src/app/contact-list/contact-list.component.ts
+++ starter-code/contact-app/src/app/contact-list/contact-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import contactList from '../contacts';
 
+
 @Component({
   selector: 'app-contact-list',
   templateUrl: './contact-list.component.html',
@@ -18,7 +19,10 @@ export class ContactListComponent implements OnInit {
 
   addContact(){
     console.log("Add contact has been called");
-    // add contact to contacts list
-    // clear inputs
+    if(this.newContact!==null){
+      this.contacts.push(this.newContact);
+      this.newContact = {};
+    }
+    
   }
 }
