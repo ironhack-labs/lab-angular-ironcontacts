c39c476a16c3088c9112f8957f54c782ad8242cb
jsavory-94 <james.pa.savory@gmail.com>
2018-02-20 15:24:00 +0100

    added validation to forms

--- starter-code/contact-app/src/app/contact-list/contact-list.component.ts
+++ starter-code/contact-app/src/app/contact-list/contact-list.component.ts
@@ -9,15 +9,22 @@ import contactList from '../contacts';
 export class ContactListComponent implements OnInit {
   contacts: Object[];
   newContact: Object = {};
-
+  feedbackEnabled: Boolean = false;
+  
   constructor() { }
 
   ngOnInit() {
     this.contacts = contactList;
   }
 
-  addContact(){
-    this.contacts.push(this.newContact);
+  addContact(form){
+    if (form.valid) {
+      this.contacts.push(this.newContact);
+      this.newContact = "";
+      this.feedbackEnabled = false;
+    } else {
+      this.feedbackEnabled = true;
+    }
 
     // add contact to contacts list
     // clear inputs
