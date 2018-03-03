76c297dc212d747250a3bae14a1a8464bef95ccc
Amelia Salgado <amelie@MacBook-Pro-de-Amelia.local>
2018-03-03 16:54:07 +0100

    feat: 3 Iterations

--- starter-code/contact-app/src/app/contact-list/contact-list.component.ts
+++ starter-code/contact-app/src/app/contact-list/contact-list.component.ts
@@ -18,7 +18,12 @@ export class ContactListComponent implements OnInit {
 
   addContact(){
     console.log("Add contact has been called");
-    // add contact to contacts list
-    // clear inputs
+    this.contacts.push(this.newContact);
+    this.newContact = {
+      name: '',
+      email: '',
+      phoneNumber: '',
+      image: ''
+    };
   }
 }
