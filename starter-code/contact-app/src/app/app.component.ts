0c8e778ffb1244354e5e7ee722c92fd62b001f76
Mario Velasco Alonso <mario1velasco@gmail.com>
2018-03-05 10:24:39 +0100

    Feat: All done

--- starter-code/contact-app/src/app/app.component.ts
+++ starter-code/contact-app/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { contacts } from './contacts';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  title = 'My lovely contacs';
+  userContacts: Object[] = [];
+  ngOnInit() {
+    this.userContacts = contacts;
+  }
 }
