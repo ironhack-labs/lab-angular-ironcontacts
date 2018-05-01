62bcedbd6bbd7b5a256db7cb8a8cc8d87e8b1680
Tanis Pan <e.pan.gar@gmail.com>
2018-05-01 16:33:05 +0200

    Iteración 2

--- starter-code/src/app/contact-list/contact-list.component.ts
+++ starter-code/src/app/contact-list/contact-list.component.ts
@@ -16,9 +16,10 @@ export class ContactListComponent implements OnInit {
     this.contacts = contactList;
   }
 
-  addContact(){
-    console.log("Add contact has been called");
-    // add contact to contacts list
+  addContact(newContact){
+    console.log(newContact);
+    this.contacts.push(newContact)
+    //newContact.name, newContact.email, newContact.phoneNumber,newContact.image
     // clear inputs
   }
 }
