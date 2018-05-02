a909c89c698b5d31da63bd93628147f986a7ab4d
Pierre Jablonski <pierre.jablonski17@imperial.ac.uk>
2018-05-02 14:22:58 +0200

    done

--- starter-code/src/app/contact-list/contact-list.component.ts
+++ starter-code/src/app/contact-list/contact-list.component.ts
@@ -8,7 +8,9 @@ import contactList from '../contacts';
 })
 export class ContactListComponent implements OnInit {
   contacts: Object[];
-  newContact: Object = {};
+  //newContact: Object = {};
+
+  newContact: Contact = new Contact();
 
   constructor() { }
 
@@ -20,5 +22,23 @@ export class ContactListComponent implements OnInit {
     console.log("Add contact has been called");
     // add contact to contacts list
     // clear inputs
+    this.newContact = {
+      name: this.newContact.name,
+      email: this.newContact.email,
+      phoneNumber: this.newContact.phoneNumber,
+      image: this.newContact.image
+    };
+
+    this.contacts.push(this.newContact);
   }
+}
+
+
+class Contact{
+  constructor (
+    public name: string = "",
+    public email: string = "", 
+    public phoneNumber: string = "",
+    public image: string = ""
+  ){}
 }
