d98129e370a075b2019db5285f02adcdebd02b0a
Randy Arbolaez <Randyarbolaez0@gmail.com>
2018-05-15 12:19:28 -0400

    finished

--- starter-code/src/app/contacts.ts
+++ starter-code/src/app/contacts.ts
@@ -3,168 +3,168 @@ const contacts: Object[] = [
     name: "Shakira Robel",
     email: "Ashley_Ritchie@hotmail.com",
     phoneNumber: "965-144-6018",
-    image: "http://lorempixel.com/640/480/sports"
+    image: "https://static.ffx.io/images/$zoom_0.4481186440677966%2C$multiply_0.25396825396825395%2C$ratio_1%2C$width_378%2C$x_329%2C$y_137/t_crop_custom/t_sharpen%2Cq_auto%2Cf_auto/72b45a949f0bcdcc2e1caa8b4c5ebebf257c7176"
   },
 
   {
     name: "Green Crist",
     email: "Rosendo53@hotmail.com",
     phoneNumber: "1-522-714-2638 x44811",
-    image: "http://lorempixel.com/640/480/food"
+    image: "https://static.ffx.io/images/$zoom_0.4481186440677966%2C$multiply_0.25396825396825395%2C$ratio_1%2C$width_378%2C$x_329%2C$y_137/t_crop_custom/t_sharpen%2Cq_auto%2Cf_auto/72b45a949f0bcdcc2e1caa8b4c5ebebf257c7176"
   },
 
   {
     name: "Yvette Dickinson",
     email: "Yolanda.Abernathy@hotmail.com",
     phoneNumber: "(038) 030-9641 x7086",
-    image: "http://lorempixel.com/640/480/nightlife"
+    image: "https://static.ffx.io/images/$zoom_0.4481186440677966%2C$multiply_0.25396825396825395%2C$ratio_1%2C$width_378%2C$x_329%2C$y_137/t_crop_custom/t_sharpen%2Cq_auto%2Cf_auto/72b45a949f0bcdcc2e1caa8b4c5ebebf257c7176"
   },
 
   {
     name: "Karley Hackett",
     email: "Icie_Gutmann69@yahoo.com",
     phoneNumber: "(251) 436-6001",
-    image: "http://lorempixel.com/640/480/nightlife"
+    image: "https://static.ffx.io/images/$zoom_0.4481186440677966%2C$multiply_0.25396825396825395%2C$ratio_1%2C$width_378%2C$x_329%2C$y_137/t_crop_custom/t_sharpen%2Cq_auto%2Cf_auto/72b45a949f0bcdcc2e1caa8b4c5ebebf257c7176"
   },
 
   {
     name: "Mrs. Otha Beahan",
     email: "Brice46@yahoo.com",
     phoneNumber: "005-851-7095 x72027",
-    image: "http://lorempixel.com/640/480/food"
+    image: "https://static.ffx.io/images/$zoom_0.4481186440677966%2C$multiply_0.25396825396825395%2C$ratio_1%2C$width_378%2C$x_329%2C$y_137/t_crop_custom/t_sharpen%2Cq_auto%2Cf_auto/72b45a949f0bcdcc2e1caa8b4c5ebebf257c7176"
   },
 
   {
     name: "Ignacio Larson",
     email: "Remington.Boyer63@yahoo.com",
     phoneNumber: "1-471-836-5871 x5841",
-    image: "http://lorempixel.com/640/480/technics"
+    image: "https://static.ffx.io/images/$zoom_0.4481186440677966%2C$multiply_0.25396825396825395%2C$ratio_1%2C$width_378%2C$x_329%2C$y_137/t_crop_custom/t_sharpen%2Cq_auto%2Cf_auto/72b45a949f0bcdcc2e1caa8b4c5ebebf257c7176"
   },
 
   {
     name: "Estel Armstrong",
     email: "Chasity.Metz83@gmail.com",
     phoneNumber: "(247) 305-7427",
-    image: "http://lorempixel.com/640/480/cats"
+    image: "https://static.ffx.io/images/$zoom_0.4481186440677966%2C$multiply_0.25396825396825395%2C$ratio_1%2C$width_378%2C$x_329%2C$y_137/t_crop_custom/t_sharpen%2Cq_auto%2Cf_auto/72b45a949f0bcdcc2e1caa8b4c5ebebf257c7176"
   },
 
   {
     name: "Citlalli Brown",
     email: "Izabella.Hand21@gmail.com",
     phoneNumber: "475.071.4608 x101",
-    image: "http://lorempixel.com/640/480/transport"
+    image: "https://static.ffx.io/images/$zoom_0.4481186440677966%2C$multiply_0.25396825396825395%2C$ratio_1%2C$width_378%2C$x_329%2C$y_137/t_crop_custom/t_sharpen%2Cq_auto%2Cf_auto/72b45a949f0bcdcc2e1caa8b4c5ebebf257c7176"
   },
 
   {
     name: "Reginald Kessler",
     email: "Retta.Wolf@gmail.com",
     phoneNumber: "070-610-2661",
-    image: "http://lorempixel.com/640/480/people"
+    image: "https://static.ffx.io/images/$zoom_0.4481186440677966%2C$multiply_0.25396825396825395%2C$ratio_1%2C$width_378%2C$x_329%2C$y_137/t_crop_custom/t_sharpen%2Cq_auto%2Cf_auto/72b45a949f0bcdcc2e1caa8b4c5ebebf257c7176"
   },
 
   {
     name: "Rosina Schiller",
     email: "Rachelle13@hotmail.com",
     phoneNumber: "869.014.6216 x103",
-    image: "http://lorempixel.com/640/480/transport"
+    image: "https://static.ffx.io/images/$zoom_0.4481186440677966%2C$multiply_0.25396825396825395%2C$ratio_1%2C$width_378%2C$x_329%2C$y_137/t_crop_custom/t_sharpen%2Cq_auto%2Cf_auto/72b45a949f0bcdcc2e1caa8b4c5ebebf257c7176"
   },
 
   {
     name: "Annabell Kreiger DVM",
     email: "Kamron_Kilback24@gmail.com",
     phoneNumber: "537.485.6536",
-    image: "http://lorempixel.com/640/480/sports"
+    image: "https://static.ffx.io/images/$zoom_0.4481186440677966%2C$multiply_0.25396825396825395%2C$ratio_1%2C$width_378%2C$x_329%2C$y_137/t_crop_custom/t_sharpen%2Cq_auto%2Cf_auto/72b45a949f0bcdcc2e1caa8b4c5ebebf257c7176"
   },
 
   {
     name: "Sister Langworth",
     email: "Mauricio.Predovic@yahoo.com",
     phoneNumber: "1-728-795-5036 x191",
-    image: "http://lorempixel.com/640/480/abstract"
+    image: "https://static.ffx.io/images/$zoom_0.4481186440677966%2C$multiply_0.25396825396825395%2C$ratio_1%2C$width_378%2C$x_329%2C$y_137/t_crop_custom/t_sharpen%2Cq_auto%2Cf_auto/72b45a949f0bcdcc2e1caa8b4c5ebebf257c7176"
   },
 
   {
     name: "Walton Cormier",
     email: "Markus25@gmail.com",
     phoneNumber: "1-218-030-7101",
-    image: "http://lorempixel.com/640/480/transport"
+    image: "https://static.ffx.io/images/$zoom_0.4481186440677966%2C$multiply_0.25396825396825395%2C$ratio_1%2C$width_378%2C$x_329%2C$y_137/t_crop_custom/t_sharpen%2Cq_auto%2Cf_auto/72b45a949f0bcdcc2e1caa8b4c5ebebf257c7176"
   },
 
   {
     name: "Mrs. Jed Anderson",
     email: "Shawn.Howell62@hotmail.com",
     phoneNumber: "1-195-147-1917",
-    image: "http://lorempixel.com/640/480/cats"
+    image: "https://static.ffx.io/images/$zoom_0.4481186440677966%2C$multiply_0.25396825396825395%2C$ratio_1%2C$width_378%2C$x_329%2C$y_137/t_crop_custom/t_sharpen%2Cq_auto%2Cf_auto/72b45a949f0bcdcc2e1caa8b4c5ebebf257c7176"
   },
 
   {
     name: "Sam Ledner",
     email: "Chase48@hotmail.com",
     phoneNumber: "701.023.2667 x499",
-    image: "http://lorempixel.com/640/480/business"
+    image: "https://static.ffx.io/images/$zoom_0.4481186440677966%2C$multiply_0.25396825396825395%2C$ratio_1%2C$width_378%2C$x_329%2C$y_137/t_crop_custom/t_sharpen%2Cq_auto%2Cf_auto/72b45a949f0bcdcc2e1caa8b4c5ebebf257c7176"
   },
 
   {
     name: "Jayson Conroy",
     email: "Ayana_Deckow38@gmail.com",
     phoneNumber: "1-130-094-5404",
-    image: "http://lorempixel.com/640/480/fashion"
+    image: "https://static.ffx.io/images/$zoom_0.4481186440677966%2C$multiply_0.25396825396825395%2C$ratio_1%2C$width_378%2C$x_329%2C$y_137/t_crop_custom/t_sharpen%2Cq_auto%2Cf_auto/72b45a949f0bcdcc2e1caa8b4c5ebebf257c7176"
   },
 
   {
     name: "Libbie Kub",
     email: "Reymundo_Brekke96@hotmail.com",
     phoneNumber: "1-632-280-6526",
-    image: "http://lorempixel.com/640/480/animals"
+    image: "https://static.ffx.io/images/$zoom_0.4481186440677966%2C$multiply_0.25396825396825395%2C$ratio_1%2C$width_378%2C$x_329%2C$y_137/t_crop_custom/t_sharpen%2Cq_auto%2Cf_auto/72b45a949f0bcdcc2e1caa8b4c5ebebf257c7176"
   },
 
   {
     name: "Tomasa Tillman",
     email: "Jordane.Wunsch@yahoo.com",
     phoneNumber: "1-946-558-9080 x542",
-    image: "http://lorempixel.com/640/480/business"
+    image: "https://static.ffx.io/images/$zoom_0.4481186440677966%2C$multiply_0.25396825396825395%2C$ratio_1%2C$width_378%2C$x_329%2C$y_137/t_crop_custom/t_sharpen%2Cq_auto%2Cf_auto/72b45a949f0bcdcc2e1caa8b4c5ebebf257c7176"
   },
 
   {
     name: "Rusty Tremblay MD",
     email: "Shana3@yahoo.com",
     phoneNumber: "773.001.9806",
-    image: "http://lorempixel.com/640/480/city"
+    image: "https://static.ffx.io/images/$zoom_0.4481186440677966%2C$multiply_0.25396825396825395%2C$ratio_1%2C$width_378%2C$x_329%2C$y_137/t_crop_custom/t_sharpen%2Cq_auto%2Cf_auto/72b45a949f0bcdcc2e1caa8b4c5ebebf257c7176"
   },
 
   {
     name: "Nina Beer",
     email: "Devon.Aufderhar74@yahoo.com",
     phoneNumber: "042.181.5876",
-    image: "http://lorempixel.com/640/480/technics"
+    image: "https://static.ffx.io/images/$zoom_0.4481186440677966%2C$multiply_0.25396825396825395%2C$ratio_1%2C$width_378%2C$x_329%2C$y_137/t_crop_custom/t_sharpen%2Cq_auto%2Cf_auto/72b45a949f0bcdcc2e1caa8b4c5ebebf257c7176"
   },
 
   {
     name: "Wayne Schaden",
     email: "Augustine46@yahoo.com",
     phoneNumber: "820-288-0927 x964",
-    image: "http://lorempixel.com/640/480/animals"
+    image: "https://static.ffx.io/images/$zoom_0.4481186440677966%2C$multiply_0.25396825396825395%2C$ratio_1%2C$width_378%2C$x_329%2C$y_137/t_crop_custom/t_sharpen%2Cq_auto%2Cf_auto/72b45a949f0bcdcc2e1caa8b4c5ebebf257c7176"
   },
 
   {
     name: "Arvel Gorczany",
     email: "Nadia.Paucek@yahoo.com",
     phoneNumber: "037.974.9471",
-    image: "http://lorempixel.com/640/480/fashion"
+    image: "https://static.ffx.io/images/$zoom_0.4481186440677966%2C$multiply_0.25396825396825395%2C$ratio_1%2C$width_378%2C$x_329%2C$y_137/t_crop_custom/t_sharpen%2Cq_auto%2Cf_auto/72b45a949f0bcdcc2e1caa8b4c5ebebf257c7176"
   },
 
   {
     name: "Carrie Connelly",
     email: "Abigail40@yahoo.com",
     phoneNumber: "1-150-622-5789 x634",
-    image: "http://lorempixel.com/640/480/cats"
+    image: "https://static.ffx.io/images/$zoom_0.4481186440677966%2C$multiply_0.25396825396825395%2C$ratio_1%2C$width_378%2C$x_329%2C$y_137/t_crop_custom/t_sharpen%2Cq_auto%2Cf_auto/72b45a949f0bcdcc2e1caa8b4c5ebebf257c7176"
   },
 
   {
     name: "Coty Grady",
     email: "Lenny_Ziemann@hotmail.com",
     phoneNumber: "(905) 672-3625 x6378",
-    image: "http://lorempixel.com/640/480/nature"
+    image: "https://static.ffx.io/images/$zoom_0.4481186440677966%2C$multiply_0.25396825396825395%2C$ratio_1%2C$width_378%2C$x_329%2C$y_137/t_crop_custom/t_sharpen%2Cq_auto%2Cf_auto/72b45a949f0bcdcc2e1caa8b4c5ebebf257c7176"
   }
 ]
 
