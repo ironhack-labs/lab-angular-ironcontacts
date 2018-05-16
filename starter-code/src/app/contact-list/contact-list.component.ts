8433b18401518c4954bc850a6816ca994dad70b7
Otton G Fernandez <ottongabriel@MacBook-Air.local>
2018-05-16 14:05:50 -0400

    week 7 day 3 work

--- starter-code/src/app/contact-list/contact-list.component.ts
+++ starter-code/src/app/contact-list/contact-list.component.ts
@@ -8,28 +8,31 @@ import contactList from '../contacts';
 })
 export class ContactListComponent implements OnInit {
   contacts: Object[];
-  
-  
-  
+
+
+
   constructor() { }
-  
+
   ngOnInit() {
     this.contacts = contactList;
   }
-  
-  
-  
-  
-  
-  
-  newContact:any = {name: '', email: '', phoneNumber: '', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg'};
+
+
+  // tslint:disable-next-line:max-line-length
+  newContact: any = {name: '', email: '', phoneNumber: '', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg'};
+
+
+  deleteTheContact(theWholeContact){
+    const index = this.contacts.indexOf(theWholeContact);
+    this.contacts.splice(index, 1);
+  }
 
 
   addContact():void{
     // add contact to contacts list
     const theNewContact:any = {name: this.newContact.name, email:  this.newContact.email, phoneNumber: this.newContact.phoneNumber, image: this.newContact.image};
 
-    this.contacts.push(theNewContact);
+    this.contacts.unshift(theNewContact);
 
     // clear inputs
 
