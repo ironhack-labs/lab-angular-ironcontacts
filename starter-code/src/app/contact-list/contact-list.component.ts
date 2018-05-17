f8af27ed4fb52925bdcef9b906d2ef82d5ba1db1
Randy Arbolaez <Randyarbolaez0@gmail.com>
2018-05-17 10:49:00 -0400

    app/movie is mistake

--- starter-code/src/app/contact-list/contact-list.component.ts
+++ starter-code/src/app/contact-list/contact-list.component.ts
@@ -4,15 +4,15 @@ import contactList from '../contacts';
 @Component({
   selector: 'app-contact-list',
   templateUrl: './contact-list.component.html',
-  styleUrls: ['./contact-list.component.css']
+  styleUrls: ['./contact-list.component.css'],
 })
 export class ContactListComponent implements OnInit {
   contacts: Array<any>;
   newContact = {
-    name: "",
-    email: "",
-    phoneNumber: "",
-    image: "",
+    name: '',
+    email: '',
+    phoneNumber: '',
+    image: '',
   };
 
   constructor() { }
@@ -21,16 +21,18 @@ export class ContactListComponent implements OnInit {
     this.contacts = contactList;
   }
 
-  addContact(): void {
+  addContact() {
     const newOne = {
       name: this.newContact.name,
       email: this.newContact.email,
       phoneNumber: this.newContact.phoneNumber,
       image: this.newContact.image,
-    }
+    };
 
-    this.contacts.push(newOne)
-    console.log("Add contact has been called");
-
+    this.contacts.unshift(newOne);
+    console.log('Add contact has been called');
+  }
+  removeContact(email) {
+    this.contacts = this.contacts.filter(contact => contact.email !== email);
   }
 }
