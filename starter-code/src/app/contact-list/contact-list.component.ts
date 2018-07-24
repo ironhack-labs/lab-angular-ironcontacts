ffa95b7c3f5a4944fbdff306b0d4b1ad31140b4f
Samuraty <alexandra.garciagasco@gmail.com>
2018-07-24 14:53:18 +0200

    Done

--- starter-code/src/app/contact-list/contact-list.component.ts
+++ starter-code/src/app/contact-list/contact-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import contactList from '../contacts';
 
 @Component({
@@ -8,17 +8,24 @@ import contactList from '../contacts';
 })
 export class ContactListComponent implements OnInit {
   contacts: Object[];
-  newContact: Object = {};
-
+  newContact: any = {};
+  
   constructor() { }
 
   ngOnInit() {
     this.contacts = contactList;
   }
 
-  addContact(){
+  addContact(nameip, emailip, phoneip, imageip){
     console.log("Add contact has been called");
     // add contact to contacts list
+    this.newContact.name = nameip;
+    this.newContact.email = emailip;
+    this.newContact.phone = phoneip;
+    this.newContact.image = imageip;
+    contactList.push(this.newContact)
+    console.log(this.newContact)
+    console.log(contactList)
     // clear inputs
   }
 }
