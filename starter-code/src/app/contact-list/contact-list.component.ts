69e35e32fc6f201490048334552f1892c429d3c6
Julien Chil Hagopian <julienchilhagopian@gmail.com>
2018-07-24 16:05:00 +0200

    done

--- starter-code/src/app/contact-list/contact-list.component.ts
+++ starter-code/src/app/contact-list/contact-list.component.ts
@@ -16,9 +16,8 @@ export class ContactListComponent implements OnInit {
     this.contacts = contactList;
   }
 
-  addContact(){
-    console.log("Add contact has been called");
-    // add contact to contacts list
-    // clear inputs
+  addContact() {
+    console.log('Add contact has been called');
+    this.contacts.push(this.newContact);
   }
 }
