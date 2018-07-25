7e82e38596de9bef6c40700da7f1f3124d3b5f98
JuanCasnova <casanovajm199621@gmail.com>
2018-07-25 07:07:48 -0500

    hecho pero al final se queda colf¿gado

---
+++ starter-code/src/app/contact.ts
@@ -0,0 +1,11 @@
+
+export class Contact {
+
+    constructor(
+      public name: string,
+      public email: string,
+      public phoneNumber: string,
+      public image: string
+    ) {  }
+  
+  }
