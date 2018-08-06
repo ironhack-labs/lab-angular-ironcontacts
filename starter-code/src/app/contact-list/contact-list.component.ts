6f78bd87415b7ca0b8a2716fca36136cb6ae16bf
me@seanzamora.com <me@seanzamora.com>
2018-08-05 21:00:14 -0400

    Completed Assignment

--- starter-code/src/app/contact-list/contact-list.component.ts
+++ starter-code/src/app/contact-list/contact-list.component.ts
@@ -1,23 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import contactList from '../contacts';
 
+interface IContactObject {
+  name: string;
+  email: string;
+  phoneNumber: string;
+  image: string;
+}
+
 @Component({
   selector: 'app-contact-list',
   templateUrl: './contact-list.component.html',
   styleUrls: ['./contact-list.component.css']
 })
 export class ContactListComponent implements OnInit {
-  contacts: Object[];
-  newContact: Object = {};
-
+  contacts: Object [];
+  newContact: IContactObject = { name: '', email: '', phoneNumber: '', image: ''};
+  errorMessage: string;
   constructor() { }
 
   ngOnInit() {
     this.contacts = contactList;
   }
 
-  addContact(){
-    console.log("Add contact has been called");
+  addContact() {
+    const {name, email, phoneNumber, image} = this.newContact;
+    this.errorMessage = '';
+    if ( name !== '' || email !== '' || phoneNumber !== '' || image !== '') {
+      this.contacts.push(this.newContact);
+    }else{
+      this.errorMessage = 'All fields are required!';
+    }
+
+    this.newContact = { name: '', email: '', phoneNumber: '', image: ''};
     // add contact to contacts list
     // clear inputs
   }
