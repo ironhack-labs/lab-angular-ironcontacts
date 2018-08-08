e21707e17ed186522b8406df8024f3b2c542e6ad
ahmouda12 <ahmouda12@ufl.edu>
2018-08-08 14:55:35 -0400

    finish iteration1

--- starter-code/src/app/contact-list/contact-list.component.ts
+++ starter-code/src/app/contact-list/contact-list.component.ts
@@ -16,9 +16,10 @@ export class ContactListComponent implements OnInit {
     this.contacts = contactList;
   }
 
-  addContact(){
+  addContact(newContact){
     console.log("Add contact has been called");
     // add contact to contacts list
+    this.contacts.push(this.newContact);
     // clear inputs
   }
 }
