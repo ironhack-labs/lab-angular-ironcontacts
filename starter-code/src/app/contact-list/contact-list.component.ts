e319b6e257b54d109792a2e3e9042bd6bb13e036
Alma Alonso <alma.aa93@gmail.com>
2018-09-08 20:50:20 +0200

    1st iteration

--- starter-code/src/app/contact-list/contact-list.component.ts
+++ starter-code/src/app/contact-list/contact-list.component.ts
@@ -1,20 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import contactList from '../contacts';
+import contacts from '../contacts';
 
 @Component({
   selector: 'app-contact-list',
-  templateUrl: './contact-list.component.html',
-  styleUrls: ['./contact-list.component.css']
+  templateUrl: './contact-list.component.html'
 })
-export class ContactListComponent implements OnInit {
-  contacts: Object[];
+export class ContactListComponent {
+  contacts: Object[] = contacts;
   newContact: Object = {};
-
-  constructor() { }
-
-  ngOnInit() {
-    this.contacts = contactList;
-  }
 
   addContact(){
     console.log("Add contact has been called");
