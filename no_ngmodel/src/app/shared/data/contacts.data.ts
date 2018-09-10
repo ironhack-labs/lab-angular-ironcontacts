562745c20240f9c0d6655b1b472a4353456d9224
fran <fmls1989@gmail.com>
2018-09-10 15:17:15 +0200

    done

---
+++ no_ngmodel/src/app/shared/data/contacts.data.ts
@@ -0,0 +1,34 @@
+import { Contact } from './../models/contact.model';
+
+const contacts: Array<Contact> = [
+    {
+      name: "Shakira Robel",
+      email: "Ashley_Ritchie@hotmail.com",
+      phoneNumber: "965-144-6018",
+      image: "https://m.media-amazon.com/images/M/MV5BMjM3NzQ5NDcxOF5BMl5BanBnXkFtZTgwNzM4MTQ5NTM@._V1_SY1000_CR0,0,679,1000_AL_.jpg"
+    },
+  
+    {
+      name: "Green Crist",
+      email: "Rosendo53@hotmail.com",
+      phoneNumber: "1-522-714-2638 x44811",
+      image: "https://m.media-amazon.com/images/M/MV5BNWVlMjQ3MjItOWE3YS00YTYwLWE0ZDMtZWMyZWY1NzkxNWIwXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SY1000_CR0,0,679,1000_AL_.jpg"
+    },
+  
+    {
+      name: "Yvette Dickinson",
+      email: "Yolanda.Abernathy@hotmail.com",
+      phoneNumber: "(038) 030-9641 x7086",
+      image: "https://m.media-amazon.com/images/M/MV5BZDVjNzhkYTItNDM4My00NWQ2LWJlNjItMGQ3OWM5OGNmYzE3XkEyXkFqcGdeQXVyMjgxOTM1MDU@._V1_SY1000_CR0,0,679,1000_AL_.jpg"
+    },
+  
+    {
+      name: "Karley Hackett",
+      email: "Icie_Gutmann69@yahoo.com",
+      phoneNumber: "(251) 436-6001",
+      image: "https://m.media-amazon.com/images/M/MV5BMjg0MzA4MDE0N15BMl5BanBnXkFtZTgwMzk3MzAwNjM@._V1_SY1000_CR0,0,679,1000_AL_.jpg"
+    }
+  ]
+  
+  export default contacts;
+  
