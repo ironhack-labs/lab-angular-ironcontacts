a88c1f65e2bcc11976a044d42867c1a50f329ec9
Marta Soler <marta@MacBook-Air-de-Barrabes.local>
2018-09-13 18:15:34 +0200

    angular app

--- starter-code/ironcontacts/src/app/app.module.ts
+++ starter-code/ironcontacts/src/app/app.module.ts
@@ -1,20 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { ContactListComponent } from './contact-list/contact-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ContactListComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
